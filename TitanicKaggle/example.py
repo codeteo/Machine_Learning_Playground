8b4db975a2c463dc4d77e90d84d1cd4bfb027978
css laptop <teo@css.laptop.gr>
2017-11-11 01:16:43 +0200

    Drops features and groups data by age

--- TitanicKaggle/example.py
+++ TitanicKaggle/example.py
@@ -8,6 +8,20 @@ data_test = pd.read_csv('data/test.csv')
 
 print(data_train.sample(3))
 
-sns.barplot(x="Embarked", y="Survived", hue="Sex", data=data_train);
+def drop_features(df):
+	return df.drop(['Ticket', 'Fare', 'Name', 'Embarked', 'Parch', 'SibSp', 'Cabin'], axis=1)
+
+def simplify_ages(df):
+	df.Age = df.Age.fillna(-0.5)
+	bins = (-1, 0, 5, 18, 60, 100)
+	group_names = ['Unknown', 'Baby', 'Teenager', 'Adult', 'Senior']
+	categories = pd.cut(df.Age, bins, labels=group_names)
+	df.Age = categories
+	return df
+
+data_train = drop_features(data_train)
+data_train = simplify_ages(data_train)
+
+sns.barplot(x="Age", y="Survived", hue="Sex", data=data_train);
 
 plt.show()
