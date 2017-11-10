4af60acfe34cb01743851eead13d5e0b5277a67b
css laptop <teo@css.laptop.gr>
2017-11-11 01:22:26 +0200

    Normalizes labels

--- TitanicKaggle/example.py
+++ TitanicKaggle/example.py
@@ -18,9 +18,22 @@ def simplify_ages(df):
 	categories = pd.cut(df.Age, bins, labels=group_names)
 	df.Age = categories
 	return df
-
+	
+from sklearn import preprocessing
+def encode_features(df_train, df_test):
+    features = ['Age', 'Sex']
+    df_combined = pd.concat([df_train[features], df_test[features]])
+    
+    for feature in features:
+        le = preprocessing.LabelEncoder()
+        le = le.fit(df_combined[feature])
+        df_train[feature] = le.transform(df_train[feature])
+        df_test[feature] = le.transform(df_test[feature])
+    return df_train, df_test
+    
 data_train = drop_features(data_train)
 data_train = simplify_ages(data_train)
+data_train, data_test = encode_features(data_train, data_test)
 
 sns.barplot(x="Age", y="Survived", hue="Sex", data=data_train);
 
