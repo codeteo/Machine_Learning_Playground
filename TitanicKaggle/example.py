642a68be44d37af9f9ab746ffbb489d9c735da7a
css laptop <teo@css.laptop.gr>
2017-11-11 02:03:36 +0200

    Predicts and prints accuracy score
    Uses RandomForest classifier

--- TitanicKaggle/example.py
+++ TitanicKaggle/example.py
@@ -18,7 +18,11 @@ def simplify_ages(df):
 	categories = pd.cut(df.Age, bins, labels=group_names)
 	df.Age = categories
 	return df
-	
+
+def simplify_sex(df):
+	df.Sex = df.Sex.fillna('NaN')
+	return df
+
 from sklearn import preprocessing
 def encode_features(df_train, df_test):
     features = ['Age', 'Sex']
@@ -33,8 +37,51 @@ def encode_features(df_train, df_test):
     
 data_train = drop_features(data_train)
 data_train = simplify_ages(data_train)
+data_train = simplify_sex(data_train)
+data_test = drop_features(data_test)
+data_test = simplify_ages(data_test)
+data_test = simplify_sex(data_test)
 data_train, data_test = encode_features(data_train, data_test)
+
+
+# Time for some ML
+from sklearn.model_selection import train_test_split
+
+X_all = data_train.drop(['Survived'], axis=1)
+y_all = data_train['Survived']
+
+num_test = 0.20
+X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size=num_test, random_state=23)
 
 sns.barplot(x="Age", y="Survived", hue="Sex", data=data_train);
 
-plt.show()
+# plt.show()
+
+# fitting and tuning
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import make_scorer, accuracy_score
+from sklearn.model_selection import GridSearchCV
+
+# choose classifier
+clf = RandomForestClassifier()
+
+params = {'n_estimators':[4, 6, 9], 'max_features':['log2', 'sqrt', 'auto'],
+			'criterion':['entropy', 'gini'], 'max_depth': [2, 3, 5, 10],
+			'min_samples_split':[2, 3, 5], 'min_samples_leaf':[1, 5, 8]
+		 }
+
+# Type of scoring used to compare parameter combinations
+acc_scorer = make_scorer(accuracy_score)
+
+# Run the grid search
+grid_obj = GridSearchCV(clf, params, scoring=acc_scorer)
+grid_obj = grid_obj.fit(X_train, y_train)
+
+# set the clf to the best combination of params
+clf = grid_obj.best_estimator_
+
+# Fit the best algorithm to the data
+clf.fit(X_train, y_train)
+
+predictions = clf.predict(X_test)
+print(accuracy_score(y_test, predictions))
